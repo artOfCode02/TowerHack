b78c2b8aa4c8dd822747a78f9516049bca46a5c1
David <dvbany10@gmail.com>
2023-11-09 17:14:14 -0500

    Added src/level.c

---
+++ src/level.c
@@ -0,0 +1,14 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const int levelXPos = 0;
+const int levelYPos = 1;
+const int levelWidth = 40;
+const int levelHeight = 40;
+
+typedef struct Level{
+  // Item ** items;
+  // Monster ** monsters;
+} Level;
