eea2a6743eecb0861c9dc04e4f0717be4777f181
David Banyeretse <dvbanyeretse@gmail.com>
2023-12-15 20:15:32 -0500

    Added Windows executable and buildWin.sh

---
+++ buildWin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm *.o
+
+echo "Compiling..."
+gcc -c src/*.c -I include
+gcc *.o -o TowerHackWin -lncurses
+
+echo "Run program? (1/0)"
+read user
+
+if [[ user -eq "1" ]] ; then
+  ./TowerHackWin
+fi
